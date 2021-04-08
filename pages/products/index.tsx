5681ebe8c0f3f8e8b3694516385f6a17a9b4daf6
Crhistian Ramirez <crhistianramirez@Crhistians-MacBook-Pro-2.local>
2021-04-07 21:29:35 -0500

    add prettier/eslint settings

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -6,8 +6,8 @@ const ProductListPage:FunctionComponent = () => {
     //This would normally be tied into the NextJS router to update OcProductList options
 
     return <div>
-        <h1>Products</h1>
-        <OcProductList />
+        <h1>Products </h1>
+            <OcProductList />
     </div>
 }
 
