e7e1c61ba1e747e48fea6535628eb27327d00bf6
Robert Watt <rwatt@four51.com>
2021-04-21 19:12:49 -0500

    Separate OcLineItemCard from OcLineItemList

--- ordercloud/components/OcLineItemList/index.tsx
+++ ordercloud/components/OcLineItemList/index.tsx
@@ -1,44 +1,6 @@
-import { LineItem } from 'ordercloud-javascript-sdk'
-import { FunctionComponent, useCallback, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { FunctionComponent } from 'react'
 import useOcCurrentOrder from '../../hooks/useOcCurrentOrder'
-import { removeLineItem } from '../../redux/ocCurrentOrder'
-import OcProductCard from '../OcProductCard'
-
-interface OcLineItemCardProps {
-  lineItem: LineItem
-}
-
-const OcLineItemCard: FunctionComponent<OcLineItemCardProps> = ({ lineItem }) => {
-  const dispatch = useDispatch()
-  const [disabled, setDisabled] = useState(false)
-
-  const handleRemoveLineItem = useCallback(async () => {
-    setDisabled(true)
-    await dispatch(removeLineItem(lineItem.ID))
-    setDisabled(false)
-  }, [dispatch, lineItem])
-
-  return (
-    <div>
-      <OcProductCard product={lineItem.Product} />
-
-      {/**
-       * TODO: Will probably need to wait to figure out product caching to make this
-       * work efficiently. Forgot price schedule isn't on line item product
-       * <OcQuantityInput priceSchedule={lineItem.Product}></OcQuantityInput>
-       * */}
-      <button
-        aria-label="Remove Line Item"
-        type="button"
-        disabled={disabled}
-        onClick={handleRemoveLineItem}
-      >
-        Remove
-      </button>
-    </div>
-  )
-}
+import OcLineItemCard from '../OcLineItemCard'
 
 const OcLineItemList: FunctionComponent = () => {
   const { lineItems } = useOcCurrentOrder()
